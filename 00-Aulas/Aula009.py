78dd516899fabb2e9a158eff2bc494f95d7b19f0
AmandaRH07 <amandarafahass13@gmail.com>
2020-10-13 20:37:56 -0300

    Aula009

--- 00-Aulas/Aula009.py
+++ 00-Aulas/Aula009.py
@@ -6,15 +6,16 @@
 #-- x = cria um novo arquivo, caso o arquivo já exista, dá erro
 #-- w = cria um novo arquivo, porem se o arquivo jé existir, apaga o antigo
 #-- a = adiciona uma linha no final do aquivo
-dict_person = {'first': "Amanda", 'last': 'Hass'}
+dict_person = {'first':"Amanda", 'last':'Hass', 'age':18}
 arquivo = open('pessoa.txt','a')
-arquivo.write(f"{dict_person['first']};{dict_person['last']}\n")
+arquivo.write(f"{dict_person['first']};{dict_person['last']};{dict_person['age']}; \n")
 arquivo.close()
 
 #== Leitura
 arquivo = open("pessoa.txt", 'r')
 for linha in arquivo:
-    linha_limpa = linha.strip() #tira caracteres especiais
+    linha_limpa = linha.strip() #-- limpa espaços em brando e caracteres de formatação (\n \t)
     lista_dados = linha_limpa.split(';')
-    print(f"nome:{lista_dados[0]} - sobrenome: {lista_dados[1]}")
+    print(f"nome:{lista_dados[0]} - sobrenome:{lista_dados[1]}, idade:{lista_dados[2]}")
+
 arquivo.close()
